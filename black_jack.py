cd0ac16ca497b6a90905c4b18d4658e7b7e4ddd3
Rafa N.A <rafaninoadiyatma0980@gmail.com>
2025-07-14 14:04:01 +0700

    cleaning code

--- black_jack.py
+++ black_jack.py
@@ -2,30 +2,24 @@ import random
 
 card_pack = ('AS',2,3,4,5,6,7,8,9,10,'J','Q','K')
 
-player_deck = []
-int_deck = []
+player_deck = {}
 
-def cards_total(deck, i = int_deck):
-    if 'AS' in deck:
-        i[-1] = 1
-        return sum(i)
-    elif 'J' in deck or 'Q' in deck or 'K' in deck:
-        i[-1] = 11
-        return sum(i)
+def cards_total(deck:dict = player_deck):
+    if 'AS' in deck.keys() and 'J' in deck.keys() or 'Q' in deck.keys() or 'K' in deck.keys():
+        return 21 + sum(deck.values()) - 12
     else:
-        return sum(i)
+        return sum(deck.values())
 
 def main():
-    while sum(int_deck) <= 21:
+    while cards_total() <= 21:
         player_pick = random.choice(card_pack)
         player_moves = input("Pick [p] or Stay [s]\n").lower()
         if player_moves == 'p':
-            player_deck.append(player_pick)
-            int_deck.append(player_pick)
+            player_deck.update({player_pick: 11 if card_pack.index(player_pick) >= 10 else card_pack.index(player_pick) + 1})
         elif player_moves == 's':
             print("You win")
             break
-        print(f'Your deck: {player_deck}\nTotal cards: {cards_total(player_deck)}')
+        print(f'Your deck: {player_deck.keys()}\nTotal cards: {cards_total()}')
     else:
         print("You lose")
 
